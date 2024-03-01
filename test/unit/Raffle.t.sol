5d19875da6df0d4363dd9c206c24720b59280e5f
EggsyOnCode <farzansaqib2003@gmail.com>
2024-03-01 15:54:42 +0500

    deploy script for raffle done

---
+++ test/unit/Raffle.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.sol";
+
+contract RaffleTest is Test{
+
+}
